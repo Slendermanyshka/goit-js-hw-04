867c5a185c3d7a52002c9c12418d5f6d326fa22c
Slendermanyshka <slendermanyshka@gmail.com>
2025-02-28 05:48:59 +0200

    initial commit

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+function isEnoughCapacity(products, containerSize) {
+  const prodAmount = Object.values(products);
+  const totalAmount = prodAmount.reduce((a,b) => a+b, 0)
+  return totalAmount <= containerSize
+  }
+  
+
+  console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));  // true
+  console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));  // false
+  console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));  // true
+  console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));  // false
